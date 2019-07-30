3833b4ab2403f7c95e9b4e09d50e428f0a291f4c
RubaSathish <53251305+RubaSathish@users.noreply.github.com>
2019-07-30 13:49:05 +0500

    Create rar3.c

---
+++ rar3.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+int main()
+{
+    int a[10],i,j,n,mid;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        mid=n/2;
+    }
+    printf("%d",a[mid]);
+    return 0;
+}
